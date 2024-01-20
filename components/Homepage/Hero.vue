59f42d1ee4e0da37b3011acee401a8fb0a326f97
Md Salim Rana <rana.salim.info@gmail.com>
2024-01-20 11:57:13 +0600

    Completed re design of hero section

--- components/Homepage/Hero.vue
+++ components/Homepage/Hero.vue
@@ -1,55 +1,73 @@
 <template>
-  <div class="py-24 md:py-36 px-4">
+  <div class="py-24 md:py-24 px-4">
     <div class="flex flex-col justify-center items-center h-full">
       <div class="text-center flex flex-col justify-center items-center">
-        <p class="text-2xl md:text-3xl text-1F1F21">Hi, I am</p>
+        <p class="text-xl md:text-3xl text-1F1F21">Hi, we are,</p>
 
-        <h1 class="text-5xl md:text-7xl font-medium mt-4 text-1F1F21">Md Salim Rana</h1>
+        <h1 class="text-4xl md:text-6xl font-medium mt-4 text-1F1F21">SR Web Studio</h1>
 
-        <p class="text-1F1F21 text-xl md:text-2xl font-semibold mt-4">
-          I do code and <span class="gradient-text">chill</span>
+        <p class="text-1F1F21 text-xl md:text-xl font-semibold mt-4">
+          We do code and <span class="gradient-text">chill</span>
         </p>
 
-        <p class="max-w-[50rem] text-xl md:text-2xl text-4E4E4E my-8">
-          Passionate Software Engineer with a focus on React Native development, dedicated to crafting elegant and
-          user-friendly mobile applications.
+        <p class="max-w-[50rem] text-xl md:text-xl text-4E4E4E mt-4">
+          We are a three person small but dedicated team of developers and designers. We are passionate about our work
+          and we love to code. We are always ready to take challenges and we are always ready to learn new things.
         </p>
 
-        <div>
+        <div class="mt-12">
           <a href="#contact" class="btn px-12">Let's Talk</a>
         </div>
       </div>
 
-      <div class="flex justify-around items-start w-full max-w-[70rem]">
-        <div>
-          <div class="mt-24 w-[12rem] md:w-[16rem] h-[12rem] md:h-[16rem] overflow-hidden rounded-full mx-auto">
+      <div class="mt-16 flex flex-col lg:flex-row justify-around items-stretch w-full max-w-[75rem] lg:space-x-4">
+        <div class="flex flex-col w-full order-3 mt-6 lg:mt-0">
+          <div class="w-[12rem] md:w-[16rem] h-[12rem] md:h-[16rem] overflow-hidden rounded-full mx-auto">
             <img src="@/assets/images/tutul.jpg" alt="" class="w-full h-full object-cover object-right-top" />
           </div>
 
-          <div class="text-center mt-6">
+          <div class="text-center mt-6 flex flex-col flex-grow">
             <h3 class="font-bold text-3xl">Abdur Rahman</h3>
-            <p class="text-xl mt-2">UI/UX Designer</p>
+            <p class="text-lg mt-2 italic font-medium">UI/UX Designer</p>
+            <p class="text-lg mb-2 italic font-medium">Team player</p>
+
+            <p class="text-lg text-4E4E4E mt-2 max-w-[25rem] mx-auto">
+              He is a UI/UX designer. He has {{ new Date().getFullYear() - 2023 }} years of experience in UI/UX design.
+            </p>
           </div>
         </div>
-        <div>
-          <div class="mt-24 w-[12rem] md:w-[16rem] h-[12rem] md:h-[16rem] overflow-hidden rounded-full mx-auto">
+
+        <div class="flex flex-col w-full order-1 lg:order-2">
+          <div class="w-[12rem] md:w-[16rem] h-[12rem] md:h-[16rem] overflow-hidden rounded-full mx-auto">
             <img src="@/assets/images/salim.jpg" alt="" class="w-full h-full object-cover object-center" />
           </div>
 
-          <div class="text-center mt-6">
+          <div class="text-center mt-6 flex flex-col flex-grow">
             <h3 class="font-bold text-3xl">Md Salim Rana</h3>
-            <p class="text-xl mt-2">Full-Stack Developer</p>
-            <p class="text-xl mt-2">Team leader</p>
+            <p class="text-lg mt-2 italic font-medium">Full-Stack Developer</p>
+            <p class="text-lg mb-2 italic font-medium">Team leader</p>
+
+            <p class="text-lg text-4E4E4E mt-2 max-w-[25rem] mx-auto">
+              He is the team leader and he is a full-stack developer. He has
+              {{ new Date().getFullYear() - 2019 }} years of experience in full-stack web development.
+            </p>
           </div>
         </div>
-        <div>
-          <div class="mt-24 w-[12rem] md:w-[16rem] h-[12rem] md:h-[16rem] overflow-hidden rounded-full mx-auto">
+
+        <div class="flex flex-col w-full order-2 lg:order-1 mt-6 lg:mt-0">
+          <div class="w-[12rem] md:w-[16rem] h-[12rem] md:h-[16rem] overflow-hidden rounded-full mx-auto">
             <img src="@/assets/images/rubel.jpg" alt="" class="w-full h-full object-cover object-right-top" />
           </div>
 
-          <div class="text-center mt-6">
+          <div class="text-center mt-6 flex flex-col flex-grow">
             <h3 class="font-bold text-3xl">Mohammad Robel</h3>
-            <p class="text-xl mt-2">Frontend Developer</p>
+            <p class="text-lg mt-2 italic font-medium">Frontend Developer</p>
+            <p class="text-lg mb-2 italic font-medium">Team player</p>
+
+            <p class="text-lg text-4E4E4E mt-2 max-w-[25rem] mx-auto">
+              He is a frontend developer. He has {{ new Date().getFullYear() - 2022 }} years of experience in frontend
+              web development.
+            </p>
           </div>
         </div>
       </div>
