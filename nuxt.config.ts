e98479c7cd352c0fab41e35691189a15fc961b24
Md Salim Rana <rana.salim.info@gmail.com>
2024-01-31 16:36:43 +0600

    removed google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,22 +4,6 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['@fortawesome/fontawesome-svg-core/styles.css'],
   app: {
-    head: {
-      script: [
-        {
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-D257LNRCHS',
-          async: true,
-        },
-        {
-          children: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-D257LNRCHS');
-          `,
-        },
-      ],
-    },
+    head: {},
   },
 })
